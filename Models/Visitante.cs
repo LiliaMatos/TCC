b0d838c369fb62201308fae5af04392bc7ae0099
Renan Guedes <renanguedesdepinho@gmail.com>
2021-03-27 20:33:55 -0300

    Alteracoes feitas no layout da tela inicial e validacoes de telefone na tela de criar visitante

--- Models/Visitante.cs
+++ Models/Visitante.cs
@@ -23,7 +23,9 @@ namespace PortariaInteligente.Models
 
         [Required(ErrorMessage = "O campo {0} é obrigatório"),
          Display(Name = "Celular"),
-         DataType(DataType.PhoneNumber)]
+         DataType(DataType.PhoneNumber),
+         RegularExpression(@"^[0-9]*$"),
+         StringLength(15, MinimumLength = 15)]
          public string VisitanteCel { get; set; }
         
         [Required(ErrorMessage = "O campo {0} é obrigatório"),
